6a9b9ecdaa45639a45c53e3f2ad4388e67d2ae09
beacortonezi <beatrizcortonezi@gmail.com>
2019-09-09 10:59:15 -0400

    Iniciando o repositório do site de receitas

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import IndexStyle from "./index.module.css"
+
+export default class Index {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
